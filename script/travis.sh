1144962c24b4a9100a3ec167ea5feb901dae607c
Travis CI <travis@travis-ci.org>
2019-02-07 21:16:42 +0100

    Being more verbose pushing to pages.

--- script/travis.sh
+++ script/travis.sh
@@ -5,3 +5,7 @@ bundle exec rspec && \
 bundle exec rubocop && \
 gem install bundler-audit && bundle-audit update && bundle-audit check && \
 gem install rubycritic && rubycritic app lib config ${RUBYCRITICPARAMS} --format console --minimum-score ${RUBYCRITICLIMIT}
+echo "Pushing badges upstream"
+[ -d badges ] || mkdir badges
+cp coverage/*.png badges/
+ls badges

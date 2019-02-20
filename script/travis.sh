42420ff288515cffca9d7d32ae3b4b4a15647883
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-20 20:46:51 +0000

    Deploy MarcGrimme/simplecov-small-badge to github.com/MarcGrimme/simplecov-small-badge.git:gh-pages

--- script/travis.sh
+++ script/travis.sh
@@ -7,8 +7,12 @@ bundle exec rspec
 bundle exec rubocop
 if [ -z "$QUICK" ]; then
   gem install bundler-audit && bundle-audit update && bundle-audit check
-  gem install rubycritic && rubycritic app lib config ${RUBYCRITICPARAMS} --format console --minimum-score ${RUBYCRITICLIMIT}
-  echo "Pushing badges upstream"
-  [ -d badges ] || mkdir badges
-  cp coverage/coverage_badge* badges/ 2>/dev/null || true
+  bundle exec rake rubycritic lib ${RUBYCRITICPARAMS}
+  # only move to the gh-pages if not a PR or branch is master
+  if [ "$TRAVIS_PULL_REQUEST" = "false" ] || [ "$TRAVIS_BRANCH" = "master" ]; then
+    echo "Pushing badges upstream"
+    [ -d badges ] || mkdir badges
+    cp coverage/coverage_badge* badges/ 2>/dev/null || true
+  fi
+
 fi

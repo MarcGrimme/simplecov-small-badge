9fcf4513b4593bcceed563e9f340db4de1b85747
Joe Yates <joe.g.yates@gmail.com>
2023-10-08 18:12:32 +0200

    Add specs for formatting branch coverage

--- spec/simplecov_small_badge_formatter_spec.rb
+++ spec/simplecov_small_badge_formatter_spec.rb
@@ -72,5 +72,129 @@ describe SimpleCovSmallBadge::Formatter do
         expect(subject.format(result)).to be_truthy
       end
     end
+
+    context 'with branch coverage' do
+      before do
+        SimpleCov.enable_coverage :branch
+        SimpleCov.primary_coverage :branch
+        allow(SimpleCov).to receive(:minimum_coverage)
+          .and_return(minimum_coverage)
+      end
+
+      after do
+        SimpleCov.clear_coverage_criteria
+        SimpleCov.enable_coverage :line
+        SimpleCov.primary_coverage :line
+      end
+
+      context 'when coverage is lower than required' do
+        let(:minimum_coverage) { { branch: 90, line: 100 } }
+        let(:coverage) { 50 }
+        let(:expected_state) { 'bad' }
+
+        it 'is bad' do
+          mock_repo_badge_image(cov: "#{coverage}%", state: expected_state)
+          result = mock_result(coverage)
+          subject.format(result)
+        end
+      end
+
+      context 'when coverage is lower than required with groups' do
+        let(:minimum_coverage) { { branch: 91, line: 100 } }
+        let(:coverage) { 90 }
+        let(:expected_state) { 'bad' }
+
+        it 'is bad' do
+          image_mock = mock_repo_badge_image(title: 'total', name: 'total',
+                                             cov: "#{coverage}%",
+                                             state: expected_state)
+          mock_repo_badge_image(title: 'library', name: 'library',
+                                cov: "#{coverage}%", state: expected_state,
+                                mock: image_mock)
+          result = mock_result(coverage,
+                               'library' => mock_result_group(coverage))
+          subject.format(result)
+        end
+      end
+
+      context 'when coverage is greater than or equal to that required' do
+        let(:minimum_coverage) { { branch: 100, line: 10 } }
+        let(:coverage) { 100 }
+        let(:expected_state) { 'good' }
+
+        it 'is good' do
+          mock_repo_badge_image(cov: '100%', state: expected_state)
+          result = mock_result(coverage)
+          subject.format(result)
+        end
+      end
+
+      context 'when coverage is greater than or equal to that required with groups' do
+        let(:minimum_coverage) { { branch: 100, line: 10 } }
+        let(:coverage) { 100 }
+        let(:expected_state) { 'good' }
+
+        it 'is good' do
+          image_mock = mock_repo_badge_image(title: 'total', name: 'total',
+                                             cov: "#{coverage}%")
+          mock_repo_badge_image(title: 'library', name: 'library',
+                                cov: "#{coverage}%", mock: image_mock)
+          result = mock_result(coverage, 'library' => mock_result_group(coverage))
+          subject.format(result)
+        end
+      end
+
+      context 'when minimum coverage is not set' do
+        let(:minimum_coverage) { {} }
+        let(:coverage) { 90 }
+        let(:expected_state) { 'unknown' }
+
+        it 'is unknown' do
+          mock_repo_badge_image(cov: "#{coverage}%",
+                                state: expected_state)
+          result = mock_result(coverage)
+          expect(subject.format(result)).to be_truthy
+        end
+      end
+
+      context 'when minimum coverage is not set with groups' do
+        let(:minimum_coverage) { {} }
+        let(:coverage) { 90 }
+        let(:expected_state) { 'unknown' }
+
+        it 'is unknown' do
+          image_mock = mock_repo_badge_image(title: 'total', name: 'total',
+                                             cov: "#{coverage}%",
+                                             state: expected_state)
+          mock_repo_badge_image(title: 'library', name: 'library',
+                                cov: "#{coverage}%", state: expected_state,
+                                mock: image_mock)
+          result = mock_result(coverage,
+                               'library' => mock_result_group(coverage))
+          expect(subject.format(result)).to be_truthy
+        end
+      end
+
+      context 'when global coverage is lower but group coverage is as required' do
+        let(:minimum_coverage) { { branch: 90, line: 10 } }
+        let(:global_coverage) { 89 }
+        let(:group_coverage) { 90 }
+        let(:global_state) { 'bad' }
+        let(:group_state) { 'good' }
+
+        it 'is mixed' do
+          image_mock = mock_repo_badge_image(title: 'total', name: 'total',
+                                             cov: "#{global_coverage}%",
+                                             state: global_state)
+          mock_repo_badge_image(title: 'library', name: 'library',
+                                cov: "#{group_coverage}%",
+                                state: group_state,
+                                mock: image_mock)
+          result = mock_result(global_coverage,
+                               'library' => mock_result_group(group_coverage))
+          subject.format(result)
+        end
+      end
+    end
   end
 end

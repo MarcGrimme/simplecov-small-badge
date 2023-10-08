d3c56b4b5271e8a239f8fb3289ef700ad1349ffd
Joe Yates <joe.g.yates@gmail.com>
2023-10-08 10:23:31 +0200

    Remove code related to SimpleCov <= 0.17

--- spec/simplecov_small_badge_formatter_spec.rb
+++ spec/simplecov_small_badge_formatter_spec.rb
@@ -8,13 +8,6 @@ describe SimpleCovSmallBadge::Formatter do
   describe '#format' do
     context 'bad result' do
       it do
-        allow(SimpleCov).to receive(:minimum_coverage).and_return(90)
-        mock_repo_badge_image(cov: '50%', state: 'bad')
-        result = mock_result(50)
-        expect(subject.format(result)).to be_truthy
-      end
-
-      it do
         allow(SimpleCov).to receive(:minimum_coverage).and_return(line: 90)
         mock_repo_badge_image(cov: '50%', state: 'bad')
         result = mock_result(50)
@@ -23,26 +16,6 @@ describe SimpleCovSmallBadge::Formatter do
     end
 
     context 'good result' do
-      it do
-        allow(SimpleCov).to receive(:minimum_coverage).and_return(100)
-        image_mock = mock_repo_badge_image(title: 'total', name: 'total',
-                                           cov: '100%')
-        mock_repo_badge_image(title: 'library', name: 'library',
-                              cov: '100%', mock: image_mock)
-        result = mock_result(100, 'library' => mock_result_group(100))
-        expect(subject.format(result)).to be_truthy
-      end
-
-      it do
-        allow(SimpleCov).to receive(:minimum_coverage).and_return(90)
-        image_mock = mock_repo_badge_image(title: 'total', name: 'total',
-                                           cov: '90%')
-        mock_repo_badge_image(title: 'library', name: 'library',
-                              cov: '90%', mock: image_mock)
-        result = mock_result(90, 'library' => mock_result_group(90))
-        expect(subject.format(result)).to be_truthy
-      end
-
       it do
         allow(SimpleCov).to receive(:minimum_coverage).and_return(line: 100)
         image_mock = mock_repo_badge_image(title: 'total', name: 'total',
@@ -66,16 +39,6 @@ describe SimpleCovSmallBadge::Formatter do
 
     context 'bad result' do
       it do
-        allow(SimpleCov).to receive(:minimum_coverage).and_return(91)
-        image_mock = mock_repo_badge_image(title: 'total', name: 'total',
-                                           cov: '90%', state: 'bad')
-        mock_repo_badge_image(title: 'library', name: 'library',
-                              cov: '90%', state: 'bad', mock: image_mock)
-        result = mock_result(90, 'library' => mock_result_group(90))
-        expect(subject.format(result)).to be_truthy
-      end
-
-      it do
         allow(SimpleCov).to receive(:minimum_coverage).and_return(line: 91)
         image_mock = mock_repo_badge_image(title: 'total', name: 'total',
                                            cov: '90%', state: 'bad')
@@ -88,16 +51,6 @@ describe SimpleCovSmallBadge::Formatter do
 
     context 'unknown result' do
       it do
-        allow(SimpleCov).to receive(:minimum_coverage).and_return(nil)
-        image_mock = mock_repo_badge_image(title: 'total', name: 'total',
-                                           cov: '90%', state: 'unknown')
-        mock_repo_badge_image(title: 'library', name: 'library',
-                              cov: '90%', state: 'unknown', mock: image_mock)
-        result = mock_result(90, 'library' => mock_result_group(90))
-        expect(subject.format(result)).to be_truthy
-      end
-
-      it do
         allow(SimpleCov).to receive(:minimum_coverage).and_return({})
         image_mock = mock_repo_badge_image(title: 'total', name: 'total',
                                            cov: '90%', state: 'unknown')
@@ -109,16 +62,6 @@ describe SimpleCovSmallBadge::Formatter do
     end
 
     context 'mixed result' do
-      it do
-        allow(SimpleCov).to receive(:minimum_coverage).and_return(90)
-        image_mock = mock_repo_badge_image(title: 'total', name: 'total',
-                                           cov: '89%', state: 'bad')
-        mock_repo_badge_image(title: 'library', name: 'library',
-                              cov: '90%', state: 'good', mock: image_mock)
-        result = mock_result(89, 'library' => mock_result_group(90))
-        expect(subject.format(result)).to be_truthy
-      end
-
       it do
         allow(SimpleCov).to receive(:minimum_coverage).and_return(line: 90)
         image_mock = mock_repo_badge_image(title: 'total', name: 'total',
